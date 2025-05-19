8e9ca151574949ac5dccad551b9cc2687c86900b
ngocdung12112000 <dungbachngoc20@gmail.com>
2025-05-19 21:33:56 +0700

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'a0cf015d7a32f4ac1a98250f12d40b2e-1631327327.ap-southeast-1.elb.amazonaws.com'
+    ]
+  }
 })
