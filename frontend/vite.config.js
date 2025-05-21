a98cbc15901034b5e58f9e311cf2a129b1ff725d
ngocdung12112000 <dungbachngoc20@gmail.com>
2025-05-21 21:32:22 +0700

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'a9131a454c62e4164bce013a5e3ea9a2-606541737.ap-southeast-1.elb.amazonaws.com/'
+      'a9131a454c62e4164bce013a5e3ea9a2-606541737.ap-southeast-1.elb.amazonaws.com'
     ]
   }
 })
